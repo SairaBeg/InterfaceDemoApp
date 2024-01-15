be9626e93e258f7ef198aea64c7bdf8a9ba374a4
SairaBeg <sairabeg.dev@gmail.com>
2024-01-14 20:57:33 -0500

    Methods called in main method

--- InterfaceDemoApp/Program.cs
+++ InterfaceDemoApp/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+abstract class Figure
+{
+    protected int Dimension;
+    public Figure() { }
+    public Figure(int dimension)
+    {
+        Dimension = dimension;
+    }
+    public abstract double Area();
+    public abstract double Perimeter();
+
+}
+
+class Circle: Figure
+{
+    public Circle() { }
+    public Circle(int radius)
+        : base(radius) { }
+    public override double Area()
+    {
+        return Math.PI * Dimension * Dimension;
+    }
+    public override double Perimeter()
+    {
+        return 2 * Math.PI * Dimension;
+    }
+}
+class Square : Figure
+{
+public Square(int side)
+        :base(side) { }
+
+    public override double Area()
+    {
+        return Dimension * Dimension;
+    }
+
+    public override double Perimeter()
+    {
+        return 4 * Dimension;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Figure fig;
+        if (args[0] == "C")
+            fig = new Circle(10);
+        else
+            fig = new Square(10);
+        Console.WriteLine(fig.Area());
+        Console.WriteLine(fig.Perimeter());
+    }
+}
